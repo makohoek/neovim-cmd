a6471e277843bb0c2da39d4d653205fb9cab40d4
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2018-06-20 15:13:28 -0700

    session: get address from environment variable
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 extern crate neovim_lib;
 
 use neovim_lib::{Neovim, NeovimApi, Session};
+use std::env;
 
 fn main() {
-    let mut session = Session::new_unix_socket("/var/folders/bx/wys6xx8j0p9_bhmrxh26_kbr0000gq/T/nvimsE9jIV/0").unwrap();
+    let socket_address = env::vars().find(|&(ref key, ref _value)| key == "NVIM_LISTEN_ADDRESS");
+
+    // TODO: use pattern matching here to handle the None case
+    let session_address = socket_address.unwrap().1;
+    println!("listening address {}", session_address);
+
+    let mut session = Session::new_unix_socket(session_address).unwrap();
     session.start_event_loop();
     let mut nvim = Neovim::new(session);
 
