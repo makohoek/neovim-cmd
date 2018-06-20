ab4073cd9781f114bf9018904ef717961fc7f155
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2018-06-20 16:43:33 -0700

    main: add some comments to explain
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,24 @@ use neovim_lib::{Neovim, NeovimApi, Session};
 use std::env;
 
 fn main() {
+    // first, check if we are within neovim's terminal (if neovim is running)
+
+    // retrieve the socket address
     let socket_address = env::vars().find(|&(ref key, ref _value)| key == "NVIM_LISTEN_ADDRESS");
-
-    // TODO: use pattern matching here to handle the None case
     let session_address = socket_address.unwrap().1;
-    println!("listening address {}", session_address);
 
+    // create a session and start it
+    // TODO: use pattern matching here to handle the None case
+    println!("listening address {}", session_address);
     let mut session = Session::new_unix_socket(session_address).unwrap();
     session.start_event_loop();
+
+    // create the nvim instance
     let mut nvim = Neovim::new(session);
 
+    // send some commands
     nvim.command("vsplit").unwrap();
 }
+
+// nnoremap <buffer> <leader>pc :Dispatch cargo build<CR>
+// nnoremap <buffer> <leader>pt :Dispatch cargo run<CR>
