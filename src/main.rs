2af489d33a086da8b16b2d94f32df1490e317bed
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2018-06-20 14:53:09 -0700

    first example with a working command in rust
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate neovim_lib;
+
+use neovim_lib::{Neovim, NeovimApi, Session};
+
 fn main() {
-    println!("Hello, world!");
+    let mut session = Session::new_unix_socket("/var/folders/bx/wys6xx8j0p9_bhmrxh26_kbr0000gq/T/nvimsE9jIV/0").unwrap();
+    session.start_event_loop();
+    let mut nvim = Neovim::new(session);
+
+    nvim.command("vsplit").unwrap();
 }
