62a0aa8580c1e00fc57c9408f63c3400c4945ec1
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2018-06-21 11:10:09 -0700

    add first argument as a file
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,39 @@ extern crate neovim_lib;
 use neovim_lib::{Neovim, NeovimApi, Session};
 use std::env;
 
+// TODO: replace by clap crate
+// https://rust-lang-nursery.github.io/rust-cookbook/app.html#ex-clap-basic
+fn usage() {
+    println!("usage: neovim_rsrc FILE");
+}
+
 fn main() {
+    // collect argc, argv[]
+    let args: Vec<String> = env::args().collect();
+
+    let filename = match args.len() {
+        2 => &args[1],
+        _ => {
+            usage();
+            return;
+        }
+    };
+
+    println!("file to open {}", filename);
+
     // first, check if we are within neovim's terminal (if neovim is running)
     let address = match env::vars().find(|&(ref key, ref _value)| key == "NVIM_LISTEN_ADDRESS") {
         // option.0 is the key (env variable name) option.1 is the value (env variable value)
         Some(option) => option.1,
         None => {
-            panic!("This only works from within a neovim terminal");
+            eprintln!("This only works from within a neovim terminal");
+            return;
         }
     };
 
     // create a session and start it
     println!("listening address {}", address);
+
     let mut session = Session::new_unix_socket(address).unwrap();
     session.start_event_loop();
 
