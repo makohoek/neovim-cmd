c6c51621aad41ebcb715f03d6491dfce4eca8db7
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2018-06-26 15:35:45 -0700

    main: rework code to handle both commands
    This is a bit of a ugly rework, but it sets the base for the following:
    - main() only handles commandline parsing, then calls a subfunction for
    each command
    - we can add other commands pretty easily
    - there are trivial commands (just send something) and more complex ones
      which need communication.
    
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate clap;
 extern crate neovim_lib;
 
-use clap::App;
+use clap::{App, SubCommand};
 use neovim_lib::{Handler, Neovim, NeovimApi, Session, Value};
 use std::env;
 use std::sync::mpsc;
@@ -39,38 +39,40 @@ impl Handler for BufferEventHandler {
     }
 }
 
-fn main() {
-    // https://rust-lang-nursery.github.io/rust-cookbook/app.html#ex-clap-basic
-    let matches = App::new("neovim-cmd")
-        .version("0.1.0")
-        .author("Mattijs Korpershoek <mattijs.korpershoek@gmail.com>")
-        .about("Send commands to neovim from the :terminal")
-        .arg_from_usage("<file> 'File to edit'")
-        .get_matches();
+fn tchdir(mut session: Session, directory: String) {
+    let command = String::from("tchdir");
+    let command = command + " ";
+    let command = command + &directory;
 
-    let filename = matches.value_of("file").unwrap();
+    session.start_event_loop();
 
-    println!("file to open {}", filename);
+    // create the nvim instance
+    let mut nvim = Neovim::new(session);
 
+    // send some commands
+    nvim.command(&command).unwrap();
+}
+
+fn edit(mut session: Session, filename: String) {
     // build the command to send to neovim
     let command = String::from("edit");
     let command = command + " ";
-    let command = command + filename;
+    let command = command + &filename;
 
-    // first, check if we are within neovim's terminal (if neovim is running)
-    let address = match env::vars().find(|&(ref key, ref _value)| key == "NVIM_LISTEN_ADDRESS") {
-        // option.0 is the key (env variable name) option.1 is the value (env variable value)
-        Some(option) => option.1,
-        None => {
-            eprintln!("This only works from within a neovim terminal");
-            return;
-        }
-    };
+    session.start_event_loop();
 
-    // create a session and start it
-    println!("listening address {}", address);
+    // create the nvim instance
+    let mut nvim = Neovim::new(session);
 
-    let mut session = Session::new_unix_socket(address).unwrap();
+    // send some commands
+    nvim.command(&command).unwrap();
+}
+
+fn edit_wait(mut session: Session, filename: String) {
+    // build the command to send to neovim
+    let command = String::from("edit");
+    let command = command + " ";
+    let command = command + &filename;
 
     let (sender, receiver) = mpsc::channel();
     session.start_event_loop_handler(BufferEventHandler(sender));
@@ -85,12 +87,7 @@ fn main() {
     println!("buffer name: {}", curbuf.get_name(&mut nvim).unwrap());
 
     // we are now subscrided to events related to this buffer
-    let attach_ok = curbuf.attach(&mut nvim, false, [].to_vec()).unwrap();
-
-    if attach_ok {
-        eprintln!("could not attach to buffer");
-        return;
-    }
+    curbuf.attach(&mut nvim, false, [].to_vec()).unwrap();
 
     // this is the receiver loop
     loop {
@@ -104,6 +101,54 @@ fn main() {
                 println!("received stuff!");
             }
         }
+    }
+}
+
+
+fn main() {
+    // https://rust-lang-nursery.github.io/rust-cookbook/app.html#ex-clap-basic
+    let matches = App::new("neovim-cmd")
+        .version("0.1.0")
+        .author("Mattijs Korpershoek <mattijs.korpershoek@gmail.com>")
+        .about("Send commands to neovim from the :terminal")
+        .subcommand(SubCommand::with_name("edit")
+                    .arg_from_usage("<file> 'File to edit'")
+                    .arg_from_usage("--wait 'Wait for buffer to be deleted'"))
+        .subcommand(SubCommand::with_name("cd")
+                    .arg_from_usage("[directory] 'Directory to :tchdir'"))
+        .get_matches();
+
+    // first, check if we are within neovim's terminal (if neovim is running)
+    let address = match env::vars().find(|&(ref key, ref _value)| key == "NVIM_LISTEN_ADDRESS") {
+        // option.0 is the key (env variable name) option.1 is the value (env variable value)
+        Some(option) => option.1,
+        None => {
+            eprintln!("This only works from within a neovim terminal");
+            return;
+        }
+    };
+
+    // create a session and start it
+    println!("listening address {}", address);
+
+    let session = Session::new_unix_socket(address).unwrap();
+
+    if let Some(matches) = matches.subcommand_matches("edit") {
+        let filename = matches.value_of("file").unwrap();
+        if matches.is_present("wait") {
+            edit_wait(session, filename.to_string());
+        } else {
+            edit(session, filename.to_string());
+        }
+        return;
+    }
+    if let Some(matches) = matches.subcommand_matches("cd") {
+        let directory = match matches.value_of("directory") {
+            Some(d) => d,
+            None => "",
+        };
+        tchdir(session, directory.to_string());
+        return;
     }
 }
 
